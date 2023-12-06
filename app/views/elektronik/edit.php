7990bc90d5a4be2ceebcb6788034a1e60a5124dd
unknown <riyansaputraoppo@gmail.com>
2023-12-06 19:33:59 +0700

    tugas_mvc

--- app/views/elektronik/edit.php
+++ app/views/elektronik/edit.php
@@ -1,19 +1,19 @@
-<h2>Edit Hewan</h2>
+<h2>Edit Elektronik</h2>
 
 <form action="<?php echo URL; ?>/elektronik/update" method="post">
     <input type="hidden" name="elektronik_id" value="<?php echo $data['row']['elektronik_id']; ?>">
     <table>
         <tr>
             <td>NAMA ELEKTRONIK</td>
-            <td><input type="text" name="nama_hewan" value="<?php echo $data['row']['nama_elektronik']; ?>" required></td>
+            <td><input type="text" name="nama_elektronik" value="<?php echo $data['row']['nama_elektronik']; ?>" required></td>
         </tr>
         <tr>
             <td>JENIS ELEKTRONIK</td>
-            <td><input type="text" name="jenis_hewan" value="<?php echo $data['row']['jenis_elektronik']; ?>"></td>
+            <td><input type="text" name="jenis_elektronik" value="<?php echo $data['row']['jenis_elektronik']; ?>"></td>
         </tr>
         <tr>
             <td>WARNA ELEKTRONIK</td>
-            <td><input type="text" name="warna_hewan" value="<?php echo $data['row']['warna_elektronik']; ?>"></td>
+            <td><input type="text" name="warna_elektronik" value="<?php echo $data['row']['warna_elektronik']; ?>"></td>
         </tr>
         <tr>
             <td></td>
