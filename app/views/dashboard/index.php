9de0dd2cba04b8598c72665c84cc464d126fbca9
unknown <riyansaputraoppo@gmail.com>
2023-12-06 19:55:31 +0700

    tugas_mvc

--- app/views/dashboard/index.php
+++ app/views/dashboard/index.php
@@ -1,3 +1,45 @@
-<h2>Dashboard</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        h2 {
+            animation: fadeInUp 1s ease-out;
+        }
 
-<div class="info">Selamat datang</div>
+        .info {
+            animation: moveInRight 1s ease-out 0.5s; /* Delay the animation */
+        }
+
+        @keyframes fadeInUp {
+            from {
+                opacity: 0;
+                transform: translateY(20px);
+            }
+            to {
+                opacity: 1;
+                transform: translateY(0);
+            }
+        }
+
+        @keyframes moveInRight {
+            from {
+                opacity: 0;
+                transform: translateX(-20px);
+            }
+            to {
+                opacity: 1;
+                transform: translateX(0);
+            }
+        }
+    </style>
+    <title>Animated Dashboard</title>
+</head>
+<body>
+
+<h2>Dashboard</h2>
+<div class="info">Selamat datang Ryan Sukandi</div>
+
+</body>
+</html>
--- app/views/elektronik/index.php
+++ app/views/elektronik/index.php
@@ -1,8 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Elektronik Table</title>
+    <style>
+        /* Add some basic styling for the table */
+        table {
+            border-collapse: collapse;
+            width: 100%;
+            margin-top: 20px;
+        }
+
+        th, td {
+            border: 1px solid #dddddd;
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #f2f2f2;
+        }
+
+        /* Add default background color for table rows */
+        tr {
+            transition: background-color 0.3s;
+        }
+
+        /* Add a highlight class for the animation effect on hover */
+        tr:hover {
+            background-color: #ffcc00;
+        }
+    </style>
+</head>
+<body>
+
 <h2>Elektronik</h2>
 
 <a href="<?php echo URL; ?>/elektronik/input" class="btn">Input elektronik</a>
 
-<table>
+<table id="elektronikTable">
     <tr>
         <th>NO</th>
         <th>NAMA ELEKTRONIK</th>
@@ -12,7 +49,8 @@
         <th>DELETE</th>
     </tr>
 
-    <?php $no = 1;
+    <?php
+    $no = 1;
     foreach ($data['rows'] as $row) { ?>
         <tr>
             <td><?php echo $no; ?></td>
@@ -22,7 +60,10 @@
             <td><a href="<?php echo URL; ?>/elektronik/edit/<?php echo $row['elektronik_id']; ?>" class="btn">Edit</a></td>
             <td><a href="<?php echo URL; ?>/elektronik/delete/<?php echo $row['elektronik_id']; ?>" class="btn" onclick="return confirm('Are you sure?')">Delete</a></td>
         </tr>
-    <?php $no++;
+    <?php
+    $no++;
     } ?>
-
 </table>
+
+</body>
+</html>
